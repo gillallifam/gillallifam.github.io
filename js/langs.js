133218fda1b520ad3634d74c1bc4ce7854fefe7d
gilmar.pereira <gilmar.pereira@itbam.org.br>
2020-12-30 17:39:58 -0400

    matchcases

--- js/langs.js
+++ js/langs.js
@@ -3,14 +3,40 @@ $Ld = {
     test: "test",
     house: "house",
     window: "window",
-    kiss: "kiss"
+    sky: "sky",
+    team: "team",
+    start: "start",
+    Start: "Start",
+    timeMsg: "Good day!",
+    defMessage: "Hello!",
+    funcMsg: () => "origin"
 }
+
 $L.BR = {
     test: "teste",
     house: "casa",
     window: "janela",
-    kiss: "beijo"
+    sky: "céu",
+    team: "time",
+    start: "iniciar",
+    Start: "Iniciar",
+    timeMsg: "Bom dia!",
+    defMessage: "Olá!",
+    funcMsg: () => { return "translated" }
 }
 $Lc = {
 
 }
+
+function matchCase (word1, word2) {
+    //   1) Words starting with two capital letters are assumed to be all caps.
+    //   2) Words starting with two lower case letters are assumed to be all lower case.
+    //   3) Words starting with an upper case followed by a lowercase letter are
+    //      all lower case except the first letter.
+  
+     return word1[0] === word1[0].toLowerCase () ? word2.toLowerCase () :
+            word1[1] === word1[1].toLowerCase () ? word2[0].toUpperCase () + word2.slice (1).toLowerCase () :
+              word2.toUpperCase ();  
+  }
+  
+ 
