7966c529fd4e7104a8d0f27d541ca6f569369dcd
mgdiferent <mgdiferent@gmail.com>
2020-12-30 06:21:24 -0400

    $L

---
+++ js/langs.js
@@ -0,0 +1,16 @@
+let $L = {}
+$Ld = {
+    test: "test",
+    house: "house",
+    window: "window",
+    kiss: "kiss"
+}
+$L.BR = {
+    test: "teste",
+    house: "casa",
+    window: "janela",
+    kiss: "beijo"
+}
+$Lc = {
+
+}
