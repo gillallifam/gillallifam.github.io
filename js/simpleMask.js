39232d4dde988dcece0ca9be0b06898d5ddc4c4b
gillallifam <gilmar.alves.ifam@gmail.com>
2021-07-10 00:39:07 -0400

    SingleMaks Demo working

--- js/simpleMask.js
+++ js/simpleMask.js
@@ -1,18 +1,19 @@
 function simpleMask() {
-    let holder = {
-        "D": (str) => /^\d+$/.test(str),
-        "A": (str) => /^[A-Za-z0-9]+$/.test(str),
-        "L": (str) => /^[A-Za-z]+$/.test(str),
+    let hdr = {
+        "D": (s) => /^\d+$/.test(s),
+        "A": (s) => /^[A-Za-z0-9]+$/.test(s),
+        "L": (s) => /^[A-Za-z]+$/.test(s),
     }
-    let symbols = Object.keys(holder)
-    let unMask = (str) => str.replace(/[^\w\s]/gi, '')
-    let maskString = (str, mask) => {
-        let raw = unMask(str)
+    let Ks = Object.keys(hdr)
+    let unMask = (s) => s.replace(/[^\w\s]/gi, '')
+    let maskString = (s, mask) => {
+        let raw = unMask(s)
         let pos = -1, res = ""
         for (const simb of mask) {
-            if (symbols.includes(simb)) {
+            if (Ks.includes(simb)) {
                 pos++
-                if (holder[simb](raw[pos])) res += raw[pos]; else break
+                if (!raw[pos]) break
+                if (hdr[simb](raw[pos])) res += raw[pos];
             } else res += simb;
         }
         return res
